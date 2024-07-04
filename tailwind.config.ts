981c1bfe680da214d28c7cb23b203c55f2537234
정필모 <itsmo1031@gmail.com>
2024-07-04 14:52:31 +0900

    🔧 chore: basic settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,16 +4,27 @@ const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/containers/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    screens: {
+      'lg': { 'max': '1023px' },
+      // => @media (max-width: 1023px) { ... }
+
+      'md': { 'max': '767px' },
+      // => @media (max-width: 767px) { ... }
+
+      'sm': { 'max': '639px' },
+      // => @media (max-width: 639px) { ... }
     },
+    container: {
+      screens: {
+        'lg': '1024px',
+        'md': '768px',
+        'sm': '640px',
+      }
+    }
   },
   plugins: [],
 };
