3b5c04571d80edffc2dd953d64b233ec80e83cbc
정필모 <itsmo1031@gmail.com>
2024-07-15 20:14:30 +0900

    💄 chore: link hover color

--- src/components/PreviewLink.tsx
+++ src/components/PreviewLink.tsx
@@ -8,7 +8,11 @@ type PreviewLinkProps = {
 const PreviewLink = ({ href, children }: PreviewLinkProps) => {
   return (
     <>
-      <a target="_blank" className="peer relative text-[#929292]" href={href}>
+      <a
+        target="_blank"
+        className="peer relative text-[#929292] underline underline-offset-4 hover:text-[#6e6e6e]"
+        href={href}
+      >
         {children}
       </a>
       <HoverPreview src="/blog.png" className="hidden peer-hover:block" />
