1d1d3895a183aa077c60c07476cba8712d197381
정필모 <itsmo1031@gmail.com>
2024-07-17 21:53:23 +0900

    ✨ feat: project component

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,4 +1,7 @@
 import { Item } from '@/interfaces/Project';
+import { getDateRangeToString } from '@/utils/date';
+import Carousel from './Carousel';
+import TechStack from './TechStack';
 
 type ProjectProps = {
   item: Item;
@@ -6,17 +9,59 @@ type ProjectProps = {
 
 const Project = ({ item }: ProjectProps) => {
   return (
-    <section
-      className="flex flex-col gap-8 border-b border-[#EEEEEE] py-10"
-    >
-      <h3 className="text-[2rem] font-bold leading-8 tracking-tighter">
-        {item.name}
-      </h3>
-      <ul className="list-inside list-disc leading-relaxed tracking-tight">
-        {item.keynotes.map((keynote, index) => (
-          <li key={index}>{keynote}</li>
-        ))}
-      </ul>
+    <section className="flex flex-col gap-5 border-b border-[#EEEEEE] py-10 md:grid-cols-1">
+      <div className="flex flex-col gap-5">
+        <div className="flex flex-col gap-2">
+          <div className="flex items-center gap-2 text-[2rem] font-bold leading-8">
+            <h3 className="tracking-tighter">{item.name}</h3>
+            {item.links?.git && (
+              <div>
+                <a
+                  href={item.links.git}
+                  target="_blank"
+                  className="flex items-center text-3xl transition-colors duration-500 hover:text-primary"
+                  rel="noreferrer"
+                  aria-label="git repository link"
+                >
+                  <span
+                    className="i-mdi-github"
+                    role="img"
+                    aria-hidden="true"
+                  />
+                </a>
+              </div>
+            )}
+          </div>
+          <div className="flex flex-col">
+            <span className="leading-tight tracking-tight">
+              {getDateRangeToString(item.startedAt, item.endedAt)}
+            </span>
+            <span className="leading-tight tracking-tight">
+              {item.position}
+            </span>
+          </div>
+          <p className="leading-normal tracking-tight">{item.description}</p>
+        </div>
+      </div>
+      {item.images && <Carousel images={item.images} />}
+      <div className="grid grid-cols-2">
+        <div>
+          <h4 className="mb-2 text-2xl font-bold tracking-tighter">
+            What did I do?
+          </h4>
+          <ul className="list-inside list-disc leading-relaxed tracking-tight">
+            {item.keynotes.map((keynote, index) => (
+              <li key={index}>{keynote}</li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <h4 className="mb-2 text-2xl font-bold tracking-tighter">
+            Tech Stack
+          </h4>
+          <TechStack data={item.skills} />
+        </div>
+      </div>
     </section>
   );
 };
