1fdc4d217da08eab0f4bf6fae769998576297e67
정필모 <itsmo1031@gmail.com>
2024-07-22 00:34:25 +0900

    ✏️ chore: change site url

--- src/config/metadata.ts
+++ src/config/metadata.ts
@@ -3,7 +3,7 @@ const meta = {
   title: '정필모 포트폴리오',
   description: '개발자 정필모 포트폴리오입니다.',
   gtag: 'G-0F14X34DBX',
-  url: 'https://resume.itsmo.dev/',
+  url: 'https://portfolio.itsmo.dev/',
   locale: 'ko_KR',
 };
 
